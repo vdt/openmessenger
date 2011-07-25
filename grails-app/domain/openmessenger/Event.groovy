a8ab9ddd9113431a2fe0e13dcb1c4c42e0c337b1
roofimon <roofimon@gmail.com>
2011-07-25 16:28:45 +0700

    Add bdd js and change defalt sort order to message and event

--- grails-app/domain/openmessenger/Event.groovy
+++ grails-app/domain/openmessenger/Event.groovy
@@ -6,13 +6,13 @@ class Event {
     Date occuredDate
     String status
 	
-    static hasMany = [subscribers:Subscriber, messages:Message]  
-	
     static constraints = {
         name(nullable: false)
         description(nullable: false)
         occuredDate(nullale: false)
         status(nullable:false, inList:['NORMAL', 'CRITICAL', 'STABLE', 'RELIVE'])
-    }
-
+    } 
+    
+    static hasMany = [subscribers:Subscriber, messages:Message] 
+    static mapping = { messages sort:'createdDate', order:'desc'}
 }
--- grails-app/domain/openmessenger/Message.groovy
+++ grails-app/domain/openmessenger/Message.groovy
@@ -4,9 +4,12 @@ class Message {
     String title
     String content     
     Date createdDate
+        
     static constraints = {
         title(nullable: false)
         content(nullable: false)
         createdDate(nullable: false)
     }
+    
+     static belongsTo = [event:Event]
 }
